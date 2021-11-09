4a5dc46cb4f54c3e3471409c8266773c5d400314
rahul <rahulrao0209@gmail.com>
2021-11-09 22:34:19 +0530

    added a home page link to the appbar

--- src/components/appbar/AppBarContainer.js
+++ src/components/appbar/AppBarContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "gatsby";
 import { AppBar, Title } from ".";
 
 export function AppBarContainer({ children }) {
   return (
     <>
       <AppBar>
-        <Title>Bookshelves</Title>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Title>Bookshelves</Title>
+        </Link>
       </AppBar>
     </>
   );
